1ab394256a609cbaee9f9fef78539d86e81ebcad
Kimd0ng <kdongh0406@gmail.com>
2025-05-15 21:36:25 +0900

    Enhance TripProgressFalseView: Update UI with a new image and improved text, increase container height, and implement view model for new trip navigation to enhance user experience.

--- client/lib/screens/view/trip_progress/trip_progress_false_view.dart
+++ client/lib/screens/view/trip_progress/trip_progress_false_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import '../../../config/color_system.dart';
 import '../../../config/font_system.dart';
 import '../../common/new_trip_button.dart';
 import '../add_trip/add_trip_view.dart';
+import '../../view_model/add_trip_view_model.dart';
+import '../../../data/repository/trip_repository.dart';
+import '../../../core/provider/trip_provider.dart';
+import 'package:provider/provider.dart';
 
 class TripProgressFalseView extends StatelessWidget {
   const TripProgressFalseView({super.key});
@@ -14,31 +17,48 @@ class TripProgressFalseView extends StatelessWidget {
       children: [
         // Placeholder for the gray box (can be replaced with an actual image or Container)
         Container(
-          height: 200,
+          height: 400,
           width: double.infinity,
-          color: Colors.grey,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: Image.asset(
+              'assets/images/new_trip_start.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
         const SizedBox(height: 16),
         // Text indicating no ongoing trip
         Text(
-          '아직 진행중인 여행이 없어요!',
-          style: AppFonts.body1,
+          'No ongoing trip!',
+          style: AppFonts.body1.copyWith(
+            color: const Color(0xFF56BC6C),
+            fontWeight: FontWeight.bold,
+          ),
         ),
         const SizedBox(height: 8),
         // Subtext
         Text(
-          '새로운 여행을 시작하고\n잊지 못할 추억을 만들어보세요.',
+          'Start a new trip and create unforgettable memories.',
           textAlign: TextAlign.center,
-          style: AppFonts.body2,
+          style: AppFonts.body2.copyWith(
+            color: const Color(0xFFB8B741),
+          ),
         ),
         const SizedBox(height: 24),
         // New Trip Button
         NewTripButton(
           onPressed: () {
+            final addTripViewModel = AddTripViewModel(
+              repository: context.read<TripRepository>(),
+              tripProvider: context.read<TripProvider>(),
+            );
             Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const AddTripView()),
-              );
+              context,
+              MaterialPageRoute(
+                builder: (_) => AddTripView(viewModel: addTripViewModel),
+              ),
+            );
           },
         ),
       ],
